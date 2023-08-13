a0c03ca4a69c30baac131efe41339dbc49876fde
Dawood malick <dawoodmalick7416@gmail.com>
2023-08-13 22:29:28 +0530

    first commit

---
+++ src/main/java/com/example/dansih/DansihApplication.java
@@ -0,0 +1,13 @@
+package com.example.dansih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DansihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DansihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dansih/DansihApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dansih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DansihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
